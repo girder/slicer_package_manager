80284c3448c2dad940c43421a3755616b2828ff3
Pierre Assemat <pierre.assemat@kitware.com>
2018-01-25 18:37:40 -0500

    Server: Add API & Models
    In this commit you can find the '/app' endpoint, and also the extension
    models.

---
+++ server/models/extension.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#  Copyright Kitware Inc.
+#
+#  Licensed under the Apache License, Version 2.0 ( the "License" );
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+##############################################################################
+
+import datetime
+import six
+
+from girder.constants import AccessType
+from girder.models.folder import Folder
+from girder.models.item import Item
+from girder.models.model_base import ValidationException
+
+
+class Extension(Item):
+    def initialize(self):
+        self.name = 'item'
+
+        self.ensureIndices(['extension_id', 'item_id'])
+        self.ensureTextIndex({
+            'name': 10,
+            'description': 1
+        })
+        self.exposeFields(level=AccessType.READ, fields=('extension_id', 'item_id', 'meta'))
+
+    def get(self, release, limit=0, offset=0, sort=None, filters=None, **kwargs):
+        """
+        Return extensions available for a release.
+        """
+        return Folder().childItems(
+            release,
+            limit=limit,
+            offset=offset,
+            sort=sort,
+            filters=filters,
+            **kwargs)
+
+    def createExtension(self, name, creator, folder, params):
+        """
+        Create and save in the DB a fresh new extension (derivative from an item with metadata).
+        """
+        item = self.createItem(
+            name,
+            creator,
+            folder,
+            params.get('description')
+        )
+        return self.setMetadata(item, params)
+
+    def validate(self, doc):
+        # Call Item validate method
+        doc = super(Extension, self).validate(doc)
+
+        if not isinstance(doc.get('created'), datetime.datetime):
+            raise ValidationException(
+                'Extension field "created" must be a datetime.')
+
+        # Validate the meta field
+        if doc.get('meta'):
+            base_params = {'app_id', 'os', 'arch', 'revision', 'repository_type', 'repository_url',
+                           'app_revision', 'packagetype', 'codebase', 'baseName', 'description'}
+            specs = []
+            for meta in base_params:
+                specs.append({
+                    'name': meta,
+                    'type': six.string_types,
+                    'exception_msg': 'Extension field "{}" must be a non-empty string.'
+                                     .format(meta)
+                })
+            for spec in specs:
+                if doc['meta'].get(spec['name']) and not isinstance(
+                   doc['meta'][spec['name']], spec['type']):
+                    raise ValidationException(spec['exception_msg'])
+            extraMeta = set(six.viewkeys(doc['meta'])) - base_params
+            if extraMeta:
+                extra_params = {'release', 'icon_url', 'development_status', 'category',
+                                'enabled', 'homepage', 'screenshots', 'contributors'}
+                if extraMeta - extra_params:
+                    raise ValidationException('Extension has extra fields: %s.' %
+                                              ', '.join(sorted(extraMeta)))
+                specs = []
+                for meta in extra_params:
+                    if meta == 'enabled':
+                        specs.append({
+                            'name': meta,
+                            'type': bool,
+                            'exception_msg': 'Extension field "{}" must be a boolean.'
+                                             .format(meta)
+                        })
+                    else:
+                        specs.append({
+                            'name': meta,
+                            'type': six.string_types,
+                            'exception_msg': 'Extension field "{}" must be a non-empty string.'
+                                             .format(meta)
+                        })
+                for spec in specs:
+                    if doc['meta'].get(spec['name']) and not isinstance(
+                       doc['meta'][spec['name']], spec['type']):
+                        raise ValidationException(spec['exception_msg'])
+            duplicateQuery = {
+                'name': doc['name'],
+                'os': doc['meta']['os'],
+                'arch': doc['meta']['arch'],
+                'revision': doc['meta']['revision']
+            }
+            if '_id' in doc:
+                duplicateQuery['_id'] = {'$ne': doc['_id']}
+            if self.findOne(duplicateQuery, fields=['_id']):
+                raise ValidationException(
+                    'An Extension with this name and characteristics already exists.')
+        return doc
