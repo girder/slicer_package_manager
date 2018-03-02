321c67ef37e3280de6d50ebb3c90f2f99f0ca7ab
Pierre Assemat <pierre.assemat@kitware.com>
2018-03-02 16:04:16 -0500

    Server: Create new API endpoint to manage application package
    This also add a new utility file.

--- server/models/package.py
+++ server/models/package.py
@@ -19,6 +19,7 @@
 import datetime
 import six
 
+from girder.models.folder import Folder
 from girder.models.item import Item
 from girder.models.model_base import ValidationException
 
@@ -34,42 +35,57 @@ class Package(Item):
         # To be able to upload within a Package the name has to stay as 'item'.
         self.name = 'item'
 
-    def createPackage(self, name, creator, folder, params):
+    def get(self, release, limit=0, offset=0, sort=None, filters=None, **kwargs):
+        """
+        Get all the application packages available for a release.
+
+        :param release: The release folder
+        :return: Generator containing all the child items of the release.
+        """
+        return Folder().childItems(
+            release,
+            limit=limit,
+            offset=offset,
+            sort=sort,
+            filters=filters,
+            **kwargs)
+
+    def createPackage(self, name, creator, folder, params, description):
         """
         Create and save in the DB a new application package.
 
         :param name: Name of the new package
         :param creator: The creator user
         :param folder: The release folder within the package will be created.
-        :param params: All the metadata to set on the new extension
-        :return: The new extension item
+        :param params: All the metadata to set on the new application package
+        :return: The new application package item
         """
         item = self.createItem(
             name,
             creator,
             folder,
-            params.get('description')
+            description
         )
         return self.setMetadata(item, params)
 
     def validate(self, doc):
         """
-        Validate the extension instance.
+        Validate the package instance.
 
-        :param doc: The extension instance
-        :return: The extension instance once validated
+        :param doc: The package instance
+        :return: The package instance once validated
         """
         # Call Item validate method
         doc = super(Package, self).validate(doc)
 
         if not isinstance(doc.get('created'), datetime.datetime):
             raise ValidationException(
-                'Extension field "created" must be a datetime.')
+                'Package field "created" must be a datetime.')
 
         # Validate the meta field
         if doc.get('meta'):
             base_params = {'app_id', 'os', 'arch', 'repository_type', 'repository_url',
-                           'app_revision', 'baseName'}
+                           'revision', 'baseName'}
             specs = []
             for meta in base_params:
                 specs.append({
@@ -85,11 +101,11 @@ class Package(Item):
                 'name': doc['name'],
                 'os': doc['meta']['os'],
                 'arch': doc['meta']['arch'],
-                'app_revision': doc['meta']['app_revision']
+                'revision': doc['meta']['revision']
             }
             if '_id' in doc:
                 duplicateQuery['_id'] = {'$ne': doc['_id']}
             if self.findOne(duplicateQuery, fields=['_id']):
                 raise ValidationException(
-                    'An Extension with this name and characteristics already exists.')
+                    'A Package with this name and characteristics already exists.')
         return doc
---
+++ server/utilities.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#  Copyright Kitware Inc.
+#
+#  Licensed under the Apache License, Version 2.0 ( the "License" );
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+##############################################################################
+from girder.models.folder import Folder
+from girder.utility.progress import ProgressContext
+
+from . import constants
+
+
+def getOrCreateReleaseFolder(application, user, app_revision):
+    release_folder = None
+    # Find the release by metadata revision
+    releases = Folder().childFolders(application, 'Folder', user=user)
+    for folder in releases:
+        if 'meta' in folder:
+            if 'revision' in folder['meta']:
+                if folder['meta']['revision'] == app_revision:
+                    release_folder = folder
+                    break
+    if not release_folder:
+        # Only the draft release in the list
+        release_folder = list(Folder().childFolders(
+            application,
+            'Folder',
+            user=user,
+            filters={'name': constants.DRAFT_RELEASE_NAME}))
+        if not release_folder:
+            raise Exception('The %s folder not found.' % constants.DRAFT_RELEASE_NAME)
+        release_folder = release_folder[0]
+
+        revision_folder = list(Folder().childFolders(
+            release_folder,
+            'Folder',
+            user=user,
+            filters={'meta.revision': app_revision}))
+        if revision_folder:
+            revision_folder = revision_folder[0]
+        else:
+            revision_folder = Folder().createFolder(
+                parent=release_folder,
+                name=app_revision,
+                parentType='Folder',
+                public=release_folder['public'],
+                creator=user)
+            revision_folder = Folder().setMetadata(
+                revision_folder,
+                {'revision': app_revision})
+        release_folder = revision_folder
+
+    return release_folder
+
+
+def deleteFolder(folder, progress, user):
+    with ProgressContext(progress, user=user,
+                         title='Deleting folder %s' % folder['name'],
+                         message='Calculating folder size...') as ctx:
+        # Don't do the subtree count if we weren't asked for progress
+        if progress:
+            ctx.update(total=Folder().subtreeCount(folder))
+        Folder().remove(folder, progress=ctx)
+    return folder
