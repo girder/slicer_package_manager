f2a567d462343542aca4f38b2210cf64e7091aa8
Pierre Assemat <pierre.assemat@kitware.com>
2018-03-05 15:23:53 -0500

    PythonClient test: Consolidate the client testing by exiting each time a cmd fail
    This commit lead to better display of each cmd tested and exit if one of
    these fail.

--- plugin_tests/python_client_tests/slicer_package_manager_client_test.sh
+++ plugin_tests/python_client_tests/slicer_package_manager_client_test.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-DEBUG=true
+DEBUG=false
 
 cmd=slicer_package_manager_client
 auth='--username admin --password adminadmin'
 
 app1Name="App1"
+app2Name="App2"
 releaseName="Release1"
 
 ext1Name="ext1"
@@ -19,35 +20,62 @@ os3="macosx"
 arch3="amd64"
 rev3="0.0.1"
 
+function check {
+    if eval $1 >/dev/null 2>&1; then
+        echo -n " ... OK"
+    else
+        echo " ...FAIL"
+        exit 1
+    fi
+}
+
+function checkDiff {
+    if [ "$?" -eq "0" ]; then
+        echo " ... OK"
+    else
+        echo " ... FAIL"
+        exit 1
+    fi
+}
+
 echo
 echo "########### CLEAN UP ###########"
-echo
-### Delete the application ###
-$cmd $auth app delete $app1Name
-echo
 
-# List application
-$cmd $auth app list
+rm -rf dwn dwn2 >/dev/null 2>&1 || true
+rm *.txt >/dev/null 2>&1 || true
 
+$cmd $auth app delete $app1Name >/dev/null 2>&1 || true
+$cmd $auth app delete $app2Name >/dev/null 2>&1 || true
 
-### Create Application ###
 echo
-echo "########### CREATE APPLICATION ###########"
+echo "########### APPLICATION ###########"
 echo
-$cmd $auth app create $app1Name --desc "This is a description for $app1Name"
+echo -n "Create applications $app1Name "
+check "$cmd $auth app create $app1Name --desc \"This is a description for $app1Name\""
+echo -n "  $app2Name"
+check "$cmd $auth app create $app2Name --desc \"This is a description for $app2Name\""
 echo
-$cmd $auth app list
+echo -n "Try to create an existing application should fail"
+($cmd $auth app create $app2Name >/dev/null 2>&1 && exit 1) || echo " ... OK"
+echo
+echo -n "List applications"
+check "$cmd $auth app list"
+echo
+echo
+echo "------ TEST APPLICATION ... OK ------"
 
-### Create release in 'App1' ###
 echo
 echo "########### CREATE RELEASE ###########"
 echo
-$cmd $auth release create $app1Name --name $releaseName --revision 0001 --desc "This is a description for $releaseName"
+echo -n "Create release $releaseName"
+check "$cmd $auth release create $app1Name --name $releaseName --revision 0001 --desc \"This is a description for $releaseName\""
 echo
-# List release from 'App1'
-$cmd $auth release list $app1Name
+echo -n "List release from 'App1'"
+check "$cmd $auth release list $app1Name"
+echo
+echo
+echo "------ TEST RELEASE ... OK ------"
 
-### Upload new extensions ###
 echo
 echo "########### UPLOAD NEW EXTENSIONS ###########"
 echo
@@ -57,56 +85,66 @@ echo 'Content of the extension 3' > file3.txt
 echo 'Content of the extension 4' > file4.txt
 echo 'Content of the extension 5' > file5.txt
 
-
 cat file*.txt > contents.txt
 
-echo UPLOAD $ext1Name
-$cmd $auth extension upload $app1Name ./file1.txt --ext_os win --arch i386 --name $ext1Name --app_revision 0000 --desc "Description for ex1" > _output1.txt
-echo UPLOAD $ext2Name
-$cmd $auth extension upload $app1Name ./file2.txt --ext_os linux --arch i386 --name $ext2Name --app_revision 0002 --desc "Description for ex2" > _output2.txt
-echo UPLOAD $ext3Name
-$cmd $auth extension upload $app1Name ./file3.txt --ext_os $os3 --arch $arch3 --name $ext3Name --app_revision $app_rev3 --revision $rev3 > _output3.txt
-echo UPLOAD $ext4Name
-$cmd $auth extension upload $app1Name ./file4.txt --ext_os linux --arch amd64 --name $ext4Name --app_revision 0000 > _output4.txt
-echo UPLOAD $ext5Name
-$cmd $auth extension upload $app1Name ./file5.txt --ext_os macosx --arch amd64 --name $ext5Name --app_revision 0000 > _output5.txt
+echo -n "UPLOAD $ext1Name"
+check "$cmd $auth extension upload $app1Name ./file1.txt --ext_os win --arch i386 --name $ext1Name --app_revision 0000 --desc \"Description for ex1\" > _output1.txt"
+echo
+echo -n "UPLOAD $ext2Name"
+check "$cmd $auth extension upload $app1Name ./file2.txt --ext_os linux --arch i386 --name $ext2Name --app_revision 0002 --desc \"Description for ex2\" > _output2.txt"
+echo
+echo -n "UPLOAD $ext3Name"
+check "$cmd $auth extension upload $app1Name ./file3.txt --ext_os $os3 --arch $arch3 --name $ext3Name --app_revision $app_rev3 --revision $rev3 > _output3.txt"
+echo
+echo -n "UPLOAD $ext4Name"
+check "$cmd $auth extension upload $app1Name ./file4.txt --ext_os linux --arch amd64 --name $ext4Name --app_revision 0000 > _output4.txt"
+echo
+echo -n "UPLOAD $ext5Name"
+check "$cmd $auth extension upload $app1Name ./file5.txt --ext_os macosx --arch amd64 --name $ext5Name --app_revision 0000 > _output5.txt"
+echo
+echo
+echo "------ UPLOAD ... OK ------"
 
-# List extensions from 'App1'
 echo
 echo "########### LIST EXTENSIONS ###########"
 echo
-echo "List all the extension in the default release:"
-$cmd $auth extension list $app1Name
+echo -n "List all the extension in the default release"
+check "$cmd $auth extension list $app1Name"
 echo
-echo "List all the extension in the $releaseName:"
-$cmd $auth extension list $app1Name --release $releaseName
+echo -n "List all the extension in the $releaseName"
+check "$cmd $auth extension list $app1Name --release $releaseName"
 echo
-echo "List all the extension in $app1Name:"
-$cmd $auth extension list $app1Name --all
-
+echo -n "List all the extension in $app1Name"
+check "$cmd $auth extension list $app1Name --all >/dev/null 2>&1"
 echo
 echo "########### DOWNLOAD EXTENSIONS ###########"
 echo
-# By ID
 echo "By ID"
 id1=$(python extractID.py _output1.txt)
 id2=$(python extractID.py _output2.txt)
 id4=$(python extractID.py _output4.txt)
 id5=$(python extractID.py _output5.txt)
 
-$cmd $auth extension download $app1Name "$id1" --dir_path ./dwn
-$cmd $auth extension download $app1Name "$id2" --dir_path ./dwn
-$cmd $auth extension download $app1Name "$id4" --dir_path ./dwn
-$cmd $auth extension download $app1Name "$id5" --dir_path ./dwn
+echo -n "DOWNLOAD $ext1Name"
+check "$cmd $auth extension download $app1Name \"$id1\" --dir_path ./dwn"
+echo
+echo -n "DOWNLOAD $ext2Name"
+check "$cmd $auth extension download $app1Name \"$id2\" --dir_path ./dwn"
+echo
+echo -n "DOWNLOAD $ext4Name"
+check "$cmd $auth extension download $app1Name \"$id4\" --dir_path ./dwn"
+echo
+echo -n "DOWNLOAD $ext5Name"
+check "$cmd $auth extension download $app1Name \"$id5\" --dir_path ./dwn"
 
 echo
 echo "By NAME"
 name3="${ext3Name}_${os3}_${arch3}_${rev3}"
-
-$cmd $auth extension download $app1Name $name3 --dir_path ./dwn
-
+echo -n "DOWNLOAD $ext3Name"
+check "$cmd $auth extension download $app1Name $name3 --dir_path ./dwn"
 echo
-echo "########### CHECKING DOWNLOADED FILES ###########"
+echo
+echo "########### COMPARE DOWNLOADED FILES ###########"
 echo
 cat dwn/*ext1* >> contents_d.txt
 cat dwn/*ext2* >> contents_d.txt
@@ -114,52 +152,56 @@ cat dwn/*ext3* >> contents_d.txt
 cat dwn/*ext4* >> contents_d.txt
 cat dwn/*ext5* >> contents_d.txt
 
-diff contents.txt contents_d.txt
 
-if [ $? != 0 ]; then
-    echo "DIFFERENT"
-    rm -rf dwn
-    rm *.txt
-    echo
-    echo
-    echo "---------------------- TEST COMPLETE : FAILURE ----------------------"
-    echo
-    exit 1
-fi
-echo "TEST OK"
-rm -rf dwn
-rm *.txt
-
-### Update extension  ### TODO
-
-# Same revision
-
-# Different revision
+echo -n "Compare content between uploaded files and downloaded files"
+check "diff contents.txt contents_d.txt"
+echo
+echo
+### Update extension  ###
+echo -n "UPDATE $ext5Name with same revision"
+check "$cmd $auth extension upload $app1Name ./file1.txt --ext_os macosx --arch amd64 --name $ext5Name --app_revision 0000"
+echo
+echo -n "DOWNLOAD $ext5Name"
+check "$cmd $auth extension download $app1Name \"$id5\" --dir_path ./dwn2"
+echo
+echo -n "The file shouldn't be updated"
+diff dwn/*ext5* dwn2/*ext*
+checkDiff
+echo
+rm -rf dwn2
+echo -n "UPDATE $ext5Name with different revision"
+check "$cmd $auth extension upload $app1Name ./file1.txt --ext_os macosx --arch amd64 --name $ext5Name --app_revision 0000 --revision 1111"
+echo
+echo -n "DOWNLOAD $ext5Name"
+check "$cmd $auth extension download $app1Name \"$id5\" --dir_path ./dwn2"
+echo
+echo -n "The file should have change"
+diff dwn/*ext1* dwn2/*ext*
+checkDiff
+echo
+echo "------ TEST EXTENSION ... OK ------"
 
 if ! $DEBUG; then
-    ### Delete extensions ###
     echo
-    echo "########### CLEAN UP ###########"
+    echo "########### DELETE ###########"
     echo
-    # By ID
-    echo "Delete by ID"
-    $cmd $auth extension delete $app1Name $id1
+    echo -n "Delete Extension by ID"
+    check "$cmd $auth extension delete $app1Name $id1"
     echo
-    # By full NAME
-    echo "Delete by NAME"
-    $cmd $auth extension delete $app1Name $name3
+    echo -n "Delete Extension by name"
+    check "$cmd $auth extension delete $app1Name $name3"
     echo
-    # List extension to make sure the delete is working
-    $cmd $auth extension list $app1Name --all
+    echo -n "Delete release"
+    check "$cmd $auth release delete $app1Name $releaseName"
     echo
-    ### Delete the application ###
-    $cmd $auth app delete $app1Name
-    echo
-
-    # List the application
-    $cmd $auth app list
+    echo -n "Delete application  $app1Name"
+    check "$cmd $auth app delete $app1Name"
+    echo -n "  $app2Name"
+    check "$cmd $auth app delete $app2Name"
     echo
 fi
 echo
 echo "---------------------- TEST COMPLETE : SUCCESS ----------------------"
 echo
+rm -rf dwn dwn2
+rm *.txt
